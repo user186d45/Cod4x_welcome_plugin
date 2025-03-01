30f949ed60f290ebfe0b543662dd383a46658cf7
user186d45 <dowhcosv@protonmail.com>
2025-03-01 16:24:15 +0330

    Add files via upload

---
+++ Cod4x_Welcome_Plugin/build/default/CMakeFiles/Cod4x_Welcome_Plugin.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for Cod4x_Welcome_Plugin.
---
+++ Cod4x_Welcome_Plugin/build/default/CMakeFiles/Cod4x_Welcome_Plugin.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cod4x_Welcome_Plugin.
+# This may be replaced when dependencies are built.
